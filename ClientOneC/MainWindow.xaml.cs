d8f5b47b7986254dc28b50dbf90150ce7d0c3d3a
DenisOrel <4uzhoy>
2015-07-01 11:18:43 +0300

    Commit project

---
+++ ClientOneC/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows;
+using System.Windows.Forms;
+using ConecctorOneC;
+
+namespace ClientOneC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+
+        public Connection Con;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            Con = new Connection();
+            Con.ConnectionString();
+       }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataGrid.ItemsSource = Con.GroupnOfNomenclatureList();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            
+            DataGrid.ItemsSource = null;
+            DataGrid.ItemsSource = Con.SearchNomenclatureName(TextBoxSearch.Text);
+            
+
+        }
+
+        private void SearchByCode_Click(object sender, RoutedEventArgs e)
+        {
+            DataGrid.ItemsSource = null;
+            DataGrid.ItemsSource = new List<Connection.Nomenclature>
+            {
+                Con.SearchNomenclatureByCode(TextBoxSearchByCode.Text)
+            };
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            Con.Nomen();
+        }
+
+    }
+    
+}
